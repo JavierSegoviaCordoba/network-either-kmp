3322cdf80086a4eb6b824a3beecc0c1c371a557b
Javier Segovia Córdoba <javiersegoviacordoba@gmail.com>
2021-01-28 11:07:20 +0100

    Fix `MalformedJsonTest`

--- projects/network/network/src/commonTest/kotlin/ktor/other/MalformedJsonTest.kt
+++ projects/network/network/src/commonTest/kotlin/ktor/other/MalformedJsonTest.kt
@@ -7,29 +7,30 @@ import com.javiersc.either.network.ktor.NetworkEither
 import com.javiersc.either.network.models.DogDTO
 import com.javiersc.either.network.models.ErrorDTO
 import com.javiersc.either.network.runTestBlocking
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.types.shouldBeTypeOf
 import io.ktor.client.request.get
-import kotlinx.serialization.SerializationException
 import kotlin.test.Test
+import kotlinx.serialization.SerializationException
 
 internal class MalformedJsonTest : BaseTest<ErrorDTO, DogDTO>() {
 
     override val codeToFile = 200 to "malformed.json"
-    override val expected = buildNetworkFailureUnknown<ErrorDTO, DogDTO>(SerializationException(message))
+    override val expected =
+        buildNetworkFailureUnknown<ErrorDTO, DogDTO>(SerializationException(partialMessage))
 
     @Test
-    fun `Request 200 with a malformed json`() = runTestBlocking {
-        NetworkEither<ErrorDTO, DogDTO> { client.get("path") }.shouldBeTypeOf<NetworkFailureUnknown>()
-            .left.throwable.message.shouldBe(expected.shouldBeTypeOf<NetworkFailureUnknown>().left.throwable.message)
-    }
+    fun `Request 200 with a malformed json`() =
+        runTestBlocking {
+            NetworkEither<ErrorDTO, DogDTO> { client.get("path") }
+                .shouldBeTypeOf<NetworkFailureUnknown>()
+                .left
+                .throwable
+                .message
+                .shouldContain(
+                    expected.shouldBeTypeOf<NetworkFailureUnknown>().left.throwable.message!!,
+                )
+        }
 }
 
-private val message =
-    """
-       |Unexpected JSON token at offset 41: Expected '}'
-       |JSON input: {
-       |  "id": 1,
-       |  "name": "Auri",
-       |  "age": 7
-    """.trimMargin()
+private const val partialMessage = "Unexpected JSON token at offset"
--- projects/network/network/src/jvmTest/kotlin/retrofit/other/MalformedJsonTest.kt
+++ projects/network/network/src/jvmTest/kotlin/retrofit/other/MalformedJsonTest.kt
@@ -7,34 +7,45 @@ import com.javiersc.either.network.models.DogDTO
 import com.javiersc.either.network.models.ErrorDTO
 import com.javiersc.either.network.retrofit.BaseTest
 import com.javiersc.either.network.runTestBlocking
-import io.kotest.matchers.shouldBe
+import io.kotest.matchers.string.shouldContain
 import io.kotest.matchers.types.shouldBeTypeOf
-import kotlinx.serialization.SerializationException
 import kotlin.test.Test
+import kotlinx.serialization.SerializationException
 
 internal class MalformedJsonTest : BaseTest<ErrorDTO, DogDTO>() {
 
     override val codeToFile: Pair<Int, String?> = 200 to "malformed.json"
-    override val expected: NetworkEither<ErrorDTO, DogDTO> = buildNetworkFailureUnknown(SerializationException(message))
+    override val expected: NetworkEither<ErrorDTO, DogDTO> =
+        buildNetworkFailureUnknown(SerializationException(partialMessage))
 
     @Test
-    fun `suspend call 200 with a malformed json`() = runTestBlocking {
-        service.getDog().shouldBeTypeOf<NetworkFailureUnknown>()
-            .left.throwable.message.shouldBe(expected.shouldBeTypeOf<NetworkFailureUnknown>().left.throwable.message)
-    }
+    fun `suspend call 200 with a malformed json`() =
+        runTestBlocking {
+            service
+                .getDog()
+                .shouldBeTypeOf<NetworkFailureUnknown>()
+                .left
+                .throwable
+                .message
+                .shouldContain(
+                    expected.shouldBeTypeOf<NetworkFailureUnknown>().left.throwable.message!!,
+                )
+        }
 
     @Test
-    fun `async call  200 with a malformed json`() = runTestBlocking {
-        service.getDogAsync().await().shouldBeTypeOf<NetworkFailureUnknown>()
-            .left.throwable.message.shouldBe(expected.shouldBeTypeOf<NetworkFailureUnknown>().left.throwable.message)
-    }
+    fun `async call  200 with a malformed json`() =
+        runTestBlocking {
+            service
+                .getDogAsync()
+                .await()
+                .shouldBeTypeOf<NetworkFailureUnknown>()
+                .left
+                .throwable
+                .message
+                .shouldContain(
+                    expected.shouldBeTypeOf<NetworkFailureUnknown>().left.throwable.message!!,
+                )
+        }
 }
 
-private val message =
-    """
-       |Unexpected JSON token at offset 41: Expected '}'
-       |JSON input: {
-       |  "id": 1,
-       |  "name": "Auri",
-       |  "age": 7
-    """.trimMargin()
+private const val partialMessage = "Unexpected JSON token at offset"
